522f6d23ee1f702c5cdf73fb1ec575cdce563b67
Akshata Ambawade <akshataambawade@gmail.com>
2022-10-28 10:26:08 +0530

    practice

---
+++ importExport/index.js
@@ -0,0 +1,6 @@
+const { add, sub, mul, div, name } = require("./operate")
+console.log(add(100, 50))
+console.log(sub(100, 50));
+console.log(mul(100, 50));
+console.log(div(100, 50));
+console.log(name)
